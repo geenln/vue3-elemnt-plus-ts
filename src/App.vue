39d67e66b74a3e32dd8dce7aa99ef605b4e39a39
genin <1546983737@qq.com>
2024-05-14 20:39:21 +0800

    chore: 添加状态库 Pinia

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {useCounterStore} from './store/modules/counter'
+
+const counterStore = useCounterStore()
+</script>
 
 <template>
   <h1>Vue3 Element Plus Ts</h1>
+  <button class="border p-2 rounded-md" @click="counterStore.increment()">
+    count加1
+  </button>
+  <div class="flex gap-2">
+    <span>{{ counterStore.count }}</span>
+    <span>{{ counterStore.doubleCount }}</span>
+  </div>
+
   <div class="flex gap-2">
     <RouterLink to="/t1" class="underline text-teal-600">TestA.vue</RouterLink>
     |
